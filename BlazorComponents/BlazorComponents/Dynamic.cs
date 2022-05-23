524fc067ab36d1854dbd7fc58e741e1352498eb3
r4gingpixel <ragingpix3l@gotoxy.pl>
2022-05-23 08:30:31 +0200

    analog clock component and tweaks to color picker

---
+++ BlazorComponents/BlazorComponents/Dynamic.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorComponents
+{
+    public class Dynamic
+    {
+        public static List<System.Reflection.Assembly> assemblies = new();
+    }
+}
--- BlazorComponents/BlazorComponents/Program.cs
+++ BlazorComponents/BlazorComponents/Program.cs
@@ -8,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 var host = builder.Build();
 //var jsRuntime = host.Services.GetRequiredService<IJSRuntime>();
 builder.Services.AddScoped(sp => new Components.ComponentsJSInterop(host.Services.GetRequiredService<IJSRuntime>()));
